bf35cc09e9a59dc73aa01b1f74e1d542e57f4d9e
Garret Diego Grant <103865764+garretdgrant@users.noreply.github.com>
2025-10-31 14:01:27 -0700

    chore: under construction (#6)

--- src/app/sweet-bakes/pre-designed/page.tsx
+++ src/app/sweet-bakes/pre-designed/page.tsx
@@ -1,9 +1,15 @@
+/*
 import PreDesignedBakesClient from "@/components/PreDesignedBakesClient";
 import { getSweetBakes } from "@/lib/fetchSanity";
+*/
+import UnderConstruction from "@/components/UnderConstruction";
 
 const SweetBakesPreDesignedPage = async () => {
+  /*
   const sweetBakes = await getSweetBakes();
   return <PreDesignedBakesClient sweetBakes={sweetBakes} />;
+  */
+  return <UnderConstruction />;
 };
 
 export default SweetBakesPreDesignedPage;
