9d3d55b55944e0279395d702b0121f74e96391fe
Garret Grant <garret.grant.swe@gmail.com>
2026-02-02 14:33:08 -0800

    chore: see if metadata fixes it

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -3,6 +3,11 @@ import type { Metadata } from "next";
 const PRODUCTION_URL = "https://www.roseandsugar.com";
 
 export const getMetadataBase = () => {
+  const vercelUrl = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+  if (vercelUrl) {
+    return new URL(`https://${vercelUrl}`);
+  }
+
   const envUrl = process.env.NEXT_PUBLIC_SITE_URL;
   if (envUrl) {
     return new URL(envUrl);
